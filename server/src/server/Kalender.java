0ceb89e849f5f1ec5728f57da0854bfda5374b8d
Henning Lehmann <34438247+henry-dv@users.noreply.github.com>
2019-06-05 13:56:16 +0200

    Hallo ich bin ein Mitarbeiter vol. 2

--- server/src/server/Kalender.java
+++ server/src/server/Kalender.java
@@ -1,4 +1,24 @@
 package server;
 
+import jdk.jshell.spi.ExecutionControl.NotImplementedException;
+import java.util.ArrayList;
+import static server.CustomExceptions.*;
+
 public class Kalender {
+    private ArrayList<Urlaubseintrag> urlaubsliste;
+
+    public Kalender(ArrayList<Urlaubseintrag> urlaubsliste) {
+        this.urlaubsliste = urlaubsliste;
+    }
+
+    public void urlaubEintragen(Urlaubseintrag eintrag) throws UrlaubNichtGenehmigtException, NotImplementedException{
+        if (!stellvertreterVorhanden(eintrag)) {
+            throw new UrlaubNichtGenehmigtException();
+        }
+        urlaubsliste.add(eintrag);
+    }
+
+    public boolean stellvertreterVorhanden(Urlaubseintrag eintrag) throws NotImplementedException {
+        throw new NotImplementedException("kommt noch");
+    }
 }
--- server/src/server/Mitarbeiter.java
+++ server/src/server/Mitarbeiter.java
@@ -1,4 +1,41 @@
 package server;
 
+import jdk.jshell.spi.ExecutionControl;
+import jdk.jshell.spi.ExecutionControl.NotImplementedException;
+
 public class Mitarbeiter {
+    private int id;
+    private String name;
+    private final int anzahlUrlaubstage;
+    private Abteilungsleiter abteilungsleiter;
+    //private String pwHash;
+    private Kalender kalender;
+
+    public Mitarbeiter (int id, String name, int anzahlUrlaubstage, Abteilungsleiter abteilungsleiter, Kalender kalender) {
+        this.id = id;
+        this.name = name;
+        this.anzahlUrlaubstage = anzahlUrlaubstage;
+        this.abteilungsleiter = abteilungsleiter;
+        this.kalender = kalender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAnzahlUrlaubstage() {
+        return anzahlUrlaubstage;
+    }
+
+    public Abteilungsleiter getAbteilungsleiter() {
+        return abteilungsleiter;
+    }
+
+    public boolean urlaubBeantragen(Urlaubseintrag eintrag) throws NotImplementedException {
+        throw new NotImplementedException("kommt bald");
+    }
 }
