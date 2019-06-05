cb5d8fc5fab0b0e2a39464bec39b7ac90544e665
Lukas Albani <lukasalbani@Lukas-MacBook-Pro.local>
2019-06-05 13:44:31 +0200

    Modify Urlaubsantrag-Klasse

--- server/src/server/Urlaubseintrag.java
+++ server/src/server/Urlaubseintrag.java
@@ -1,4 +1,47 @@
 package server;
 
+import java.time.LocalDate;
+
 public class Urlaubseintrag {
+
+    // Attribute
+    private int id;
+    private int mitarbeiterId;
+    private LocalDate beginn;
+    private LocalDate ende;
+    private boolean genehmigt;
+
+    //Konstruktor
+    public Urlaubseintrag(int id, int mitarbeiterId, LocalDate beginn, LocalDate ende) {
+        this.id = id;
+        this.mitarbeiterId = mitarbeiterId;
+        this.beginn = beginn;
+        this.ende = ende;
+    }
+
+    // getter-Methoden
+    public int getId() {
+        return id;
+    }
+
+    public int getMitarbeiterId() {
+        return mitarbeiterId;
+    }
+
+    public LocalDate getBeginn() {
+        return beginn;
+    }
+
+    public LocalDate getEnde() {
+        return ende;
+    }
+
+    public boolean istGenehmigt() {
+        return genehmigt;
+    }
+
+    // setter-Methoden
+    public void setGenehmigt(boolean genehmigt) {
+        this.genehmigt = genehmigt;
+    }
 }
